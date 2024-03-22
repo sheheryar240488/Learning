663389b47abd9433f83cc98327525a993555db88
Sheher Yar Shah <shehheryar@gmail.com>
2024-03-22 06:31:41 +0100

    How to create variables in python

---
+++ Python/1.Variables.py
@@ -0,0 +1,18 @@
+"""
+Variables are containers for storing data values.
+
+Creating Variables
+Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it.
+"""
+
+# for example
+
+name = "Ali"
+age = 35
+weight = 84.5
+
+print("My name is ", name)
+print("I am ", age, "years old")
+print ("My weight is ", weight, "kg")
